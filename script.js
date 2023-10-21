dd64a9d9b6eb584722b9e99ab8f05eea930bd4d3
maxjeanty <maxjeanty@gmail.com>
2023-10-21 12:55:05 -0500

    cleaned up JS logic to make

--- script.js
+++ script.js
@@ -25,29 +25,31 @@ function isValidEmail(email) {
     return re.test(String(email).toLowerCase());
 }
 
+
+// checking required field
+function checkRequired(inputArr) {
+    inputArr.forEach(function(input){
+        if (input.value.trim() ==="") {
+            showError(input, `${getFieldName(input)} is required`)
+        }else{
+            showSuccess(input)
+        }
+    });
+}
+
+
+
+
+function getFieldName(input){
+    return input.id.charAt(0).toUpperCase()+ input.id.slice(1)
+}
+
+
+
 //listening to events
 form.addEventListener('submit', function(e){
     e.preventDefault();
-    if(username.value === ""){
-        showError(username,'Usernmae is required')
-    }else{
-        showSuccess(username)
-    }
-    if(email.value === ""){
-        showError(email,'email is required')
-    }else if(!isValidEmail(email.value)) {
-        showError(email,'email not valid')
-    }else{
-        showSuccess(email)
-    }
-    if(password.value === ""){
-        showError(password,'password is required')
-    }else{
-        showSuccess(password)
-    }
-    if(password2.value === ""){
-        showError(password2,'password2 is required')
-    }else{
-        showSuccess(password2)
-    }
+
+    checkRequired([username,email, password,password2])
+  
 })
