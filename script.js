3c57ea809d4c2d446a4c19234318a121dc850ad7
maxjeanty <maxjeanty@gmail.com>
2023-10-21 01:05:19 -0500

    added regex to email validation

--- script.js
+++ script.js
@@ -18,6 +18,13 @@ function showSuccess(input){
     formControl.className='form-control success'
 }
 
+
+//check valid email
+function isValidEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 //listening to events
 form.addEventListener('submit', function(e){
     e.preventDefault();
@@ -28,6 +35,8 @@ form.addEventListener('submit', function(e){
     }
     if(email.value === ""){
         showError(email,'email is required')
+    }else if(!isValidEmail(email.value)) {
+        showError(email,'email not valid')
     }else{
         showSuccess(email)
     }
